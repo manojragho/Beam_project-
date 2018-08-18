8aec2553f7db853732374db46ae474b1a4382938
manoragho <mano.ragho@gmail.com>
2018-08-17 22:04:06 -0400

    tweet logs will be saved to a file

--- Credentials.py
+++ Credentials.py
@@ -1,3 +1,5 @@
+# These are main access code for interaction with twitter. These info. are confidential.
+
 Access_Token = "747099887855009793-uMSGwE1f7rbodWNuJUBgHyERHYyWzVl"
 Access_Token_Secret = "Gwzv6kQANKuEwqzncGdXzU4Xw6AMNi9ih8FCQfkZPxraD"
 Consumer_Key = "W3VIg1KRNgnXO98ufnJNwK5YS"
--- first file.py
+++ first file.py
@@ -1,3 +1,4 @@
+# I am using tweepy package for interaction with twitter API
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -6,17 +7,25 @@ import Credentials
 
 class Listen(StreamListener):
 
+    def __init__(self,filename):
+        self.filename = filename
+
     def on_data(self, a):
-        print (a)
+        with open(self.filename,'a') as write_file:
+            write_file.write(a)
+
+        #print (a)
 
     def on_error(self, b):
         print(b)
 
 
 if __name__=="__main__":
-    a_Listen = Listen()
+    a_Listen = Listen("tweets.txt")
     authenticate = OAuthHandler(Credentials.Consumer_Key,Credentials.Consumer_Secret)
     authenticate.set_access_token(Credentials.Access_Token,Credentials.Access_Token_Secret)
 
     streamer = Stream(authenticate,a_Listen)
     streamer.filter(track=['trump'])
+    #a_Listen.filename = "tweets.txt"
+
